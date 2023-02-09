9b4625327c61f9aae4e6ab6f69445ad9dee247a2
pp2_22B030495 <123387665+pp2-22B03495@users.noreply.github.com>
2023-02-09 18:00:31 +0600

    Update Task5.py

--- tsis3/Classes/Task5.py
+++ tsis3/Classes/Task5.py
@@ -9,17 +9,18 @@ class BankAccount:
     def withdraw(self, amount):
         if self.balance >= amount:
             self.balance -= amount
+            print("Остаток: ")
             return self.balance
         else:
             print("Insufficient funds")
             return self.balance
 
-name = input()
-balance = int(input())
+name = input('Имя ' )
+balance = int(input('Баланс '))
 acct1 = BankAccount(name, balance)
-acct1.deposit(int(input()))
-acct1.withdraw(int(input())) 
-acct1.withdraw(int(input())) 
+acct1.deposit(int(input('Депозит: ')))
+acct1.withdraw(int(input('Сколько хотите снять: '))) 
+acct1.withdraw(int(input('Сколько хотите снять: '))) 
     
     
 print(acct1.balance) 
