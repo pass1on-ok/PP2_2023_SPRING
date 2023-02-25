58597e24306dd2c40da7e9a234b27150a2085c37
Kumar Oskenbay <123387665+pp2-22B03495@users.noreply.github.com>
2023-02-25 17:49:44 +0600

    Delete tsis5/RegEX directory

--- tsis5/RegEX/Task10.py
+++
@@ -1,9 +0,0 @@
-import re
-
-camel_string = 'HelloWorldHowAreYou'
-
-
-snake_string = re.sub(r'(?<!^)(?=[A-Z])', '_',camel_string).lower()
-
-print('Original string:', camel_string)
-print('Snake case string:', snake_string)
